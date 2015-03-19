bfd89f7302227a3d27ddcfa9e519a34a8194b596
Denes Bodo <bod.denes@gmail.com>
2015-03-19 21:48:55 +0100

    REM: setDirection from RailwayConnection

--- command-center/src/main/java/hu/denes/command_center/roco_connection/PrintoutConnection.java
+++ command-center/src/main/java/hu/denes/command_center/roco_connection/PrintoutConnection.java
@@ -2,31 +2,31 @@ package hu.denes.command_center.roco_connection;
 
 public class PrintoutConnection implements RailwayConnection {
 
+	@Override
 	public void setSpeed(final int address, final int speed) {
 		System.out.println("Speed set to " + speed + " @ " + address);
 
 	}
 
-	public void setDirection(final int address, final DIRECTION direction) {
-		System.out.println("Direction set to " + direction + " @ " + address);
-
-	}
-
+	@Override
 	public void turnLightsOn(final int address) {
 		System.out.println("Lights turned on @ " + address);
 
 	}
 
+	@Override
 	public void turnLightsOff(final int address) {
 		System.out.println("Lights turned off @ " + address);
 
 	}
 
+	@Override
 	public void turnFunctionOn(final int address, final int function) {
 		System.out.println(function + " turned on @ " + address);
 
 	}
 
+	@Override
 	public void turnFunctionOff(final int address, final int function) {
 		System.out.println(function + " turned off @ " + address);
 
--- command-center/src/main/java/hu/denes/command_center/roco_connection/RailwayConnection.java
+++ command-center/src/main/java/hu/denes/command_center/roco_connection/RailwayConnection.java
@@ -1,14 +1,8 @@
 package hu.denes.command_center.roco_connection;
 
-
 public interface RailwayConnection {
-	public enum DIRECTION {
-		FORWARD, BACKWARD
-	}
 
 	public void setSpeed(int address, int speed);
-
-	public void setDirection(int address, DIRECTION direction);
 
 	public void turnLightsOn(int address);
 
